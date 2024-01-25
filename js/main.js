19239549bac61ee20609e7ad57a9c7d34570d652
zhura <zhuravlevkos9@gmail.com>
2024-01-25 19:05:59 +0700

    Комментарии к коду

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
+// Определение модуля для работы с локальным хранилищем
 const storageModule = {
+  // Ключ для сохранения данных в локальном хранилище
   storageKey: 'notes-app',
+  // Метод для получения данных из локального хранилища
   getData() {
     const data = localStorage.getItem(this.storageKey);
     return data ? JSON.parse(data) : { firstColumn: [], secondColumn: [], thirdColumn: [] };
   },
+  // Метод для сохранения данных в локальное хранилище
   saveData(data) {
     localStorage.setItem(this.storageKey, JSON.stringify(data));
   }
 };
 
+// Определение Vue-компонента 'card'
 Vue.component('card', {
+  // Входные параметры (props) компонента
   props: ['cardData', 'disableEdit'],
+  // Методы компонента
   methods: {
+    // Метод для обновления прогресса выполнения карточки
     updateProgress() {
       const checkedCount = this.cardData.items.filter(item => item.checked).length;
       const progress = (checkedCount / this.cardData.items.length) * 100;
@@ -19,16 +27,17 @@ Vue.component('card', {
       if (this.cardData.isComplete) {
         this.cardData.lastChecked = new Date().toLocaleString();
       }
+      // Вызов событий для обновления прогресса и сохранения данных
       this.$emit('update-progress');
       this.$emit('save-data');
     },
-
+    // Метод для удаления группы карточек
     deleteGroup() {
       this.$emit('delete-group', this.cardData.id);
       this.$emit('save-data');
     }
-    
   },
+  // HTML-шаблон компонента
   template: `
     <div class="card">
       <h3>{{ cardData.groupName }}</h3>
@@ -44,8 +53,11 @@ Vue.component('card', {
   `
 });
 
+// Создание корневого экземпляра Vue
 new Vue({
+  // Привязка Vue-экземпляра к элементу с id "app"
   el: '#app',
+  // Исходные данные Vue-экземпляра и инициализация столбцов
   data: {
     ...storageModule.getData(),
     groupName: null,
@@ -57,6 +69,7 @@ new Vue({
       { title: 'Список №3', cards: [] }
     ]
   },
+  // Наблюдатель за изменениями в столбцах для автоматического сохранения данных
   watch: {
     columns: {
       handler() {
@@ -69,23 +82,24 @@ new Vue({
       deep: true
     }
   },
+  // Методы Vue-экземпляра, включая методы из модуля storageModule
   methods: {
     ...storageModule,
+    // Метод для удаления группы карточек по идентификатору
+    deleteGroup(groupId) {
+      const indexFirst = this.firstColumn.findIndex(group => group.id === groupId);
+      const indexSecond = this.secondColumn.findIndex(group => group.id === groupId);
+      const indexThird = this.thirdColumn.findIndex(group => group.id === groupId);
 
-      deleteGroup(groupId) {
-        const indexFirst = this.firstColumn.findIndex(group => group.id === groupId);
-        const indexSecond = this.secondColumn.findIndex(group => group.id === groupId);
-        const indexThird = this.thirdColumn.findIndex(group => group.id === groupId);
-
-        if (indexFirst !== -1) {
-          this.firstColumn.splice(indexFirst, 1);
-        } else if (indexSecond !== -1) {
-          this.secondColumn.splice(indexSecond, 1);
-        } else if (indexThird !== -1) {
-          this.thirdColumn.splice(indexThird, 1);
-        }
-
-      },
+      if (indexFirst !== -1) {
+        this.firstColumn.splice(indexFirst, 1);
+      } else if (indexSecond !== -1) {
+        this.secondColumn.splice(indexSecond, 1);
+      } else if (indexThird !== -1) {
+        this.thirdColumn.splice(indexThird, 1);
+      }
+    },
+    // Метод для перемещения карточек между столбцами с учетом прогресса
     moveColumn(fromColumn, toColumn, progressThreshold, maxToColumnLength) {
       fromColumn.forEach(card => {
         const progress = (card.items.filter(item => item.checked).length / card.items.length) * 100;
@@ -95,6 +109,7 @@ new Vue({
         }
       });
     },
+    // Метод для перемещения карточек из первого столбца во второй
     MoveFirstColm() {
       if (this.secondColumn.length === 5 && this.firstColumn.some(card => {
         const progress = (card.items.filter(item => item.checked).length / card.items.length) * 100;
@@ -106,33 +121,36 @@ new Vue({
         this.moveColumn(this.firstColumn, this.secondColumn, 50, 5);
       }
     },
-
+    // Метод для перемещения карточек из второго столбца в третий
     MoveSecondColm() {
       this.moveColumn(this.secondColumn, this.thirdColumn, 100, Infinity);
       this.MoveFirstColm();
     },
+    // Метод для проверки и выполнения перемещения карточек
     checkMoveCard() {
       this.MoveFirstColm();
       this.MoveSecondColm();
     },
-   
-  addCard() {
-    const nonNullInputs = this.inputs.filter(input => input !== null);
-    if (nonNullInputs.length >= 3 && nonNullInputs.length <= 5 && this.groupName && !this.isFirstColumnBlocked) {
-      if (this.firstColumn.length < 3) {
-        this.firstColumn.push({ id: Date.now(), groupName: this.groupName, items: nonNullInputs.map(text => ({ text, checked: false })) });
-        this.groupName = null;
-        this.inputs = [null, null, null, null, null];
-        this.inputsCount = 0;
+    // Метод для добавления новой карточки с группой
+    addCard() {
+      const nonNullInputs = this.inputs.filter(input => input !== null);
+      if (nonNullInputs.length >= 3 && nonNullInputs.length <= 5 && this.groupName && !this.isFirstColumnBlocked) {
+        if (this.firstColumn.length < 3) {
+          this.firstColumn.push({ id: Date.now(), groupName: this.groupName, items: nonNullInputs.map(text => ({ text, checked: false })) });
+          this.groupName = null;
+          this.inputs = [null, null, null, null, null];
+        }
       }
-    }
-    this.checkMoveCard();
-    this.saveData({
-      firstColumn: this.firstColumn,
-      secondColumn: this.secondColumn,
-      thirdColumn: this.thirdColumn
-    });
-  },
+      // Проверка и выполнение перемещения карточек
+      this.checkMoveCard();
+      // Сохранение данных после добавления карточки
+      this.saveData({
+        firstColumn: this.firstColumn,
+        secondColumn: this.secondColumn,
+        thirdColumn: this.thirdColumn
+      });
+    },
+    // Метод для загрузки данных из локального хранилища
     loadData() {
       const data = this.getData();
       this.firstColumn = data.firstColumn;
@@ -140,6 +158,7 @@ new Vue({
       this.thirdColumn = data.thirdColumn;
     },
   },
+  // Логика, выполняемая после монтажа Vue-экземпляра
   mounted() {
     this.loadData();
     this.checkMoveCard();
